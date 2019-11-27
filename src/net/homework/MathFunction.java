ece11fb9cc9a59caec1aa1eb07688d94dc65740a
Alexandr 13K <kuchersasha.ruz@gmail.com>
2019-11-27 13:10:19 +0200

    Adding new class MathFunction

---
+++ src/net/homework/MathFunction.java
@@ -0,0 +1,12 @@
+package net.homework;
+
+public class MathFunction {
+    static void main(String[] args) {
+
+        int a = 324;
+        double b;
+        float c = 2.4f;
+        b=a*c;
+        System.out.println(a);
+    }
+}
