5f75fd015798155206a67a5bdf8297698bc53f7a
Alexandr 13K <kuchersasha.ruz@gmail.com>
2019-11-27 15:39:13 +0200

    Add new function

--- src/net/homework/MathFunction.java
+++ src/net/homework/MathFunction.java
@@ -4,9 +4,11 @@ public class MathFunction {
     static void main(String[] args) {
 
         int a = 324;
-        double b;
+        double b,d,s;
         float c = 2.4f;
         b=a*c;
+        d=a/c;
+        s=a+c;
         System.out.println(a);
     }
 }
