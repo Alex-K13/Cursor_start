d0ec201dc5d4fe9a46632e98263edbe81f453125
Alexandr 13K <kuchersasha.ruz@gmail.com>
2019-11-27 17:42:18 +0200

    Add new fuction

--- src/net/homework/MathFunction.java
+++ src/net/homework/MathFunction.java
@@ -1,7 +1,7 @@
 package net.homework;
 
 public class MathFunction {
-    static void main(String[] args) {
+   public static void main(String[] args) {
 
         int a = 324;
         double b,d,s;
@@ -9,6 +9,9 @@ public class MathFunction {
         b=a*c;
         d=a/c;
         s=a+c;
-        System.out.println(a);
+        System.out.println("множення= "+b);
+        System.out.println("ділення= "+d);
+        System.out.println("сума= " +s);
+
     }
 }
